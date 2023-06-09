cc0fbc71512c071d252aa0689b8675f4f45a7de1
ezypolis.senapahlevi <sena.pahlevi@ezypolis.com>
2023-06-09 15:59:20 +0700

    openstreet map

--- pages/header.jsx
+++ pages/header.jsx
@@ -7,24 +7,7 @@ function Header() {
     <div class="navbar-1-1">
       <div class="container mx-auto flex flex-wrap flex-row items-center justify-between">
        
-          <svg
-            width="42"
-            height="42"
-            viewBox="0 0 42 42"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              fill-rule="evenodd"
-              clip-rule="evenodd"
-              d="M3.5 15.75C3.5 8.98451 8.98451 3.5 15.75 3.5H29.75C30.7165 3.5 31.5 4.2835 31.5 5.25C31.5 6.2165 30.7165 7 29.75 7H15.75C10.9175 7 7 10.9175 7 15.75V29.75C7 30.7165 6.2165 31.5 5.25 31.5C4.2835 31.5 3.5 30.7165 3.5 29.75V15.75Z"
-              fill="#0EC8F8"
-            />
-            <path
-              d="M10.5 17.5C10.5 13.634 13.634 10.5 17.5 10.5H31.5C35.366 10.5 38.5 13.634 38.5 17.5V31.5C38.5 35.366 35.366 38.5 31.5 38.5H17.5C13.634 38.5 10.5 35.366 10.5 31.5V17.5Z"
-              fill="#0EC8F8"
-            />
-          </svg>
+         
         <label for="menu-toggle" class="cursor-pointer lg:hidden block">
           <svg
             class="w-6 h-6"
